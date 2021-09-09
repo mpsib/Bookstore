403ef6af0a5f41e273e8759eeb08f00f61ec83a6
Markus Sibakov <markus.sibakov@myy.haaga-helia.fi>
2021-09-09 03:08:36 +0300

    first commit

---
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package com.example.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class BookController {
+
+	@RequestMapping("/index")
+	public String page() {
+		return "book";
+	}
+}
